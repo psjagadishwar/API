ef023e5358d5a2012fb5ebacab60b996329bd7c1
psjagadishwar <psjagadishwar@gmail.com>
2022-10-13 15:34:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ import mysql.connector
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] ='jags1234'
-app.config['MYSQL_DATABASW_DB'] = 'cricketers'
+app.config['MYSQL_DATABASE_DB'] = 'cricketers'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
